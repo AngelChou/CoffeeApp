3a6f27e4f62f876244b1ff172efca01baa41957d
Shun-Ching <Angel@AngeldeMacBook-Pro.local>
2017-05-06 17:33:30 +0800

    Complete ViewController.swift

--- CoffeeApp/ViewController.swift
+++ CoffeeApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var waterTextField: UITextField!
+    @IBOutlet weak var ratioTextField: UITextField!
+    @IBOutlet weak var coffeeTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        if let water:Float = Float(waterTextField.text!) {
+            if let ratio:Float = Float(ratioTextField.text!){
+                let coffee:Float = water/ratio
+               coffeeTextField.text = String(coffee)
+            }
+        }
+        
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
